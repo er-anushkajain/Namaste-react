eac3b6b76a9e6de85e461e446107220d602fc0cb
er-anushkajain <jainpalak0605@gmail.com>
2026-04-29 00:29:53 +0530

    adding changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,34 +1,47 @@
-import { useEffect } from "react";
-import Shimmer from "./Shimmer";
+import { useEffect, useState } from 'react';
+import Shimmer from './Shimmer';
+import { useParams } from 'react-router';
+import { MENU_API_URL } from '../../utils/commom';
 
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
 
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.8554984&lng=78.7902154&restaurantId=1234261&submitAction=ENTER",
-    );
+    const data = await fetch(MENU_API_URL + resId);
+
     const result = await data.json();
     console.log(`restaurant menu ${JSON.stringify(result)}`);
-    setResInfo(json.data);
+
+    setResInfo(JSON.stringify(result));
   };
 
   if (resInfo === null) return <Shimmer />;
+  const { name, cuisines, costForTwoMessage } =
+    resInfo.data.cards[5].groupedCard.cardGroupMap.REGULAR.cards[1].card.card
+      .itemCards[3].card.info;
+  const { itemCards } =
+    resInfo?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card?.itemCards;
+
   return (
     <div className="menu">
-      <h1>Name of the Restaurant</h1>
-      <h2>Menu</h2>
+      <h1>{name}</h1>
+      <h3>{cuisines.join(',')}</h3>
+      <h4>{costForTwoMessage} . rs</h4>)<h2>Menu</h2>
       <ul>
-        <li>Pizza</li>
-        <li>Burger</li>
-        <li>French Fries</li>
-        <li>Coke</li>
-        <li>Pepsi</li>
-        <li>Ice Cream</li>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>{item.card.info.name}</li>
+        ))}
+        <li>
+          {item.card.info.name} .{' '}
+          {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+        </li>
       </ul>
     </div>
   );
